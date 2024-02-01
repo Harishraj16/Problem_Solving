c71434c4eaa33e634e72fe1875695e01d0d1e2db
harish_selva <harishselvaraj27@gmail.com>
2024-02-01 07:00:03 +0530

    Update _02.MissingNumber.java

--- SeanParker/Arrays/_02.MissingNumber.java
+++ SeanParker/Arrays/_02.MissingNumber.java
@@ -9,3 +9,16 @@ class Solution {
         return target-sum;
     }
 }
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int xor1 = 0;
+        int xor2 = 0;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            xor2 ^= nums[i];
+            xor1 ^= (i+1);
+        }
+        return xor1^xor2;
+    }
+}
