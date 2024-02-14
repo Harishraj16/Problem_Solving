835d037fdb98e89350c17975d17a5039e157cfb0
harish_selva <harishselvaraj27@gmail.com>
2024-02-14 19:45:25 +0530

    Update _07.Sort012s.java

--- CodeIO/Daily_Dose/_07.Sort012s.java
+++ CodeIO/Daily_Dose/_07.Sort012s.java
@@ -19,3 +19,30 @@ class Solution
         }
     }
 }
+
+//Optimal way:
+class Solution
+{
+    public static void sort012(int a[], int n)
+    {
+        int low=0;
+        int mid=0;
+        int high=n-1;
+        while(mid<=high){
+            if(a[mid]==0){
+                int temp = a[mid];
+                a[mid]=a[low];
+                a[low]=temp;
+                low++;
+                mid++;
+            }
+            else if(a[mid]==1) mid++;
+            else{
+                int temp = a[mid];
+                a[mid] = a[high];
+                a[high] = temp;
+                high--;
+            }
+        }
+    }
+}
