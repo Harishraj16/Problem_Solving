44e30950a028900235ef4c2a2ddc3486dc945312
harish_selva <harishselvaraj27@gmail.com>
2024-02-14 17:33:41 +0530

    Update _01.FindTransitionPoint.cpp

--- CodeIO/Daily_Dose/_01.FindTransitionPoint.cpp
+++ CodeIO/Daily_Dose/_01.FindTransitionPoint.cpp
@@ -9,3 +9,24 @@ public:
         return -1;
     }
 };
+
+//Optimal Code:
+class Solution
+{
+public:    
+    
+    int transitionPoint(int arr[], int n) {
+        int start = 0;
+        int end = n-1;
+        int res=-1;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if(arr[mid]==1){
+                end = mid-1;
+                res = mid;
+            }
+            else start=mid+1;
+        }
+        return res;
+    }
+};
