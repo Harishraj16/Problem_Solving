5723974c1c3c496bec2307161d14b8e8613d5f45
harish_selva <harishselvaraj27@gmail.com>
2024-02-19 17:05:49 +0530

    Create _01.PowerOfTwo.java

---
+++ LeetCode/_01.PowerOfTwo.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        for(int i=0;i<32;i++){
+            if(Math.pow(2,i) == n) return true;
+        }
+        return false;
+    }
+}
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+       if (n <= 0) {
+            return false;
+        }
+        if (n == 1) {
+            return true;
+        }
+        if (n % 2 == 1) {
+            return false;
+        }
+        return isPowerOfTwo(n / 2);
+        
+    }
+}
+
+
