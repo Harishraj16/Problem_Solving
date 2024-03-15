c3a1e0ed116a2233c0353d9be7b0970db079c8b9
harish_selva <harishselvaraj27@gmail.com>
2024-03-15 19:29:52 +0530

    Create _07.PartitionArrayAccordingtoGivenPivot.java\

---
+++ LeetCode/_07.PartitionArrayAccordingtoGivenPivot.java
@@ -0,0 +1,23 @@
+//2161. Partition Array According to Given Pivot
+
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+        int n = nums.length;
+        int s = 0, m=0,l=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]<pivot) s++;
+            else if(nums[i]==pivot) m++;
+            else l++;
+        }
+        int ans[] = new int[n];
+        l=s+m;
+        m=s;
+        s=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]<pivot) ans[s++]=nums[i];
+            else if(nums[i]==pivot) ans[m++]=nums[i];
+            else ans[l++] = nums[i]; 
+        }
+        return ans;
+    }
+}
