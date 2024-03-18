4ea4aa54384bc7635c5eac4e4a7865523c9df249
harish_selva <harishselvaraj27@gmail.com>
2024-03-18 07:14:19 +0530

    Create _09.MaximumProductSubarray.java

---
+++ LeetCode/_09.MaximumProductSubarray.java
@@ -0,0 +1,20 @@
+//152. Maximum Product Subarray
+
+class Solution {
+    public int maxProduct(int[] nums) {
+        int pro = 1;
+        int maxi = Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            pro = pro*nums[i];
+            maxi = maxi>pro?maxi:pro;
+            if(pro==0)pro=1;
+        }
+        pro = 1;
+        for(int i=nums.length-1;i>=0;i--){
+            pro = pro*nums[i];
+            maxi = maxi>pro?maxi:pro;
+            if(pro==0)pro=1;
+        }
+        return maxi;
+    }
+}
