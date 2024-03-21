3d4dcf9da38d8bef5add37cf49d50b06509835f1
harish_selva <harishselvaraj27@gmail.com>
2024-03-21 16:09:12 +0530

    Create 04.FinalValueofVariableAfterPerformingOperations.java

---
+++ LeetCode/Arrays/04.FinalValueofVariableAfterPerformingOperations.java
@@ -0,0 +1,11 @@
+//2011.Final Value of Variable After Performing Operations
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int x = 0;
+        for(int i=0;i<operations.length;i++){
+            if(operations[i].charAt(1)=='+') x+=1;
+            else x-=1;
+        }
+        return x;
+    }
+}
