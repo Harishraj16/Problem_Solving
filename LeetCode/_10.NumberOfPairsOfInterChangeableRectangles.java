bf9fe1dbd7fa2180fb73b3a5d8a703cdc7745fd2
harish_selva <harishselvaraj27@gmail.com>
2024-03-21 16:00:29 +0530

    Create _10.NumberOfPairsOfInterChangeableRectangles.java

---
+++ LeetCode/_10.NumberOfPairsOfInterChangeableRectangles.java
@@ -0,0 +1,16 @@
+//2001. Number of Pairs of Interchangeable Rectangles
+class Solution {
+    public long interchangeableRectangles(int[][] nums) {
+        int n = nums.length;
+        int m = nums[0].length;
+        int k=0;
+        long count = 0;
+        HashMap<Double,Long> mpp = new HashMap<>();
+        for(int i=0;i<n;i++){
+            double div = (double)nums[i][0]/nums[i][1];
+            if(mpp.containsKey(div)) count+=mpp.get(div);
+            mpp.put(div,mpp.getOrDefault(div,0L)+1);
+        }
+        return count;
+    }
+}
