dd2fa740167cd789cd56fec77b37c706d9f4075d
harish_selva <harishselvaraj27@gmail.com>
2024-03-29 23:45:42 +0530

    Create _03.TwoSum.java

---
+++ NeetCode150/_03.TwoSum.java
@@ -0,0 +1,20 @@
+//1.Two Sum
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        int[] ans = new int[2];
+        ans[0] = ans[1] = -1;
+        HashMap<Integer,Integer> mpp = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int needed = target-nums[i];
+            if(mpp.containsKey(needed)){
+                ans[0] = mpp.get(needed);
+                ans[1] = i;
+                return ans;
+            }
+            mpp.put(nums[i],i);
+        }
+        return ans;
+    }
+}
