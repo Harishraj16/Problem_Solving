45951f609bc80590063cedb471327cf99d2a8134
harish_selva <harishselvaraj27@gmail.com>
2024-06-06 23:32:50 +0530

    Update 02.CountPairSum.cpp

--- CodeIO/Amazon/02.CountPairSum.cpp
+++ CodeIO/Amazon/02.CountPairSum.cpp
@@ -1,15 +1,15 @@
 class Solution
 {
   public:
-    int countPairs(int arr1[], int arr2[],  int m, int n, int x) 
+    int countPairs(int arr[], int arr1[],  int m, int n, int x) 
     { 
         int count = 0;
         set<int> s;
         for(int i=0;i<n;i++){
-            s.insert(arr2[i]);
+            s.insert(arr1[i]);
         }
         for(int i=0;i<m;i++){
-            int rem = x-arr1[i];
+            int rem = x-arr[i];
             if(s.find(rem)!=s.end()) count++;
         }
         return count;
